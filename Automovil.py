e45acb27391e901303d66a59bc25e53456d6191a
valen178 <valentino.cortese@davinci.edu.ar>
2024-09-30 11:31:27 -0300

    first commit

---
+++ Automovil.py
@@ -0,0 +1,7 @@
+class Automovil:
+    ruedas = 4
+    def __init__(self,color,marca,aceleracion,velocidad):
+        self.color = color
+        self.marca = marca
+        self.aceleracion = aceleracion
+        self.velocidad = velocidad
